8fd06e8accc4824e468926ab37ca6edff450fa0e
KiwiFarah <warnakulasuriyafarah@gmail.com>
2022-11-12 18:18:04 +0400

    hard code the weather data for dubai

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
+// const url = "https://api.openweathermap.org/data/2.5/weather?lat=25.031797&lon=55.191002&appid=9f5f7a36e1ab5440db689a0e77e6a9f2"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
-     
+    <div className="app">
+      <div className="container"></div>
+      <div className="top">
+        <div className="location">
+          <p>Dubai</p>
+        </div>
+        <div className="temp">
+          <h1>304.05°F</h1>
+        </div>
+        <div className="description">
+          <p>Clear</p>
+        </div>
+      </div>
+      <div className="bottom"></div>
+      <div className="feels">
+        <p>304.7°F</p>
+      </div>
+      <div className="humidity">
+        <p>45%</p>
+      </div>
+      <div className="wind">
+        4.63 MPH
+      </div>
     </div>
   );
 }
