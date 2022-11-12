e40a5284a41f81999177228b5e0d1beebcee16ed
KiwiFarah <warnakulasuriyafarah@gmail.com>
2022-11-12 20:11:39 +0400

    integrate openmap api and style components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,70 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-// const url = "https://api.openweathermap.org/data/2.5/weather?lat=25.031797&lon=55.191002&appid=9f5f7a36e1ab5440db689a0e77e6a9f2"
 
 function App() {
+const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=9f5f7a36e1ab5440db689a0e77e6a9f2`
+
+
+
+const searchLocation = (event) => {
+  if (event.key === 'Enter') {
+    axios.get(url).then((response) => {
+      setData(response.data)
+      console.log(response.data)
+    })
+    setLocation('')
+  }
+}
+
   return (
     <div className="app">
-      <div className="container"></div>
+    <div className="search">
+      <input
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Enter Location'
+        type="text" />
+    </div>
+    <div className="container">
       <div className="top">
         <div className="location">
-          <p>Dubai</p>
+          <p>{data.name}</p>
         </div>
         <div className="temp">
-          <h1>304.05°F</h1>
+          {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
         </div>
         <div className="description">
-          <p>Clear</p>
+          {data.weather ? <p>{data.weather[0].main}</p> : null}
         </div>
       </div>
-      <div className="bottom"></div>
-      <div className="feels">
-        <p>304.7°F</p>
-      </div>
-      <div className="humidity">
-        <p>45%</p>
-      </div>
-      <div className="wind">
-        4.63 MPH
-      </div>
+
+      {data.name !== undefined &&
+        <div className="bottom">
+          <div className="feels">
+            {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      }
+
+
+
     </div>
-  );
+  </div>
+);
 }
 
 export default App;
